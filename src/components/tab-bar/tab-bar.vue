0d180c2e23f22bdbbdb410b9c3291f0709c02ffa
Feng, Shun <723278075@qq.com>
2024-03-05 18:22:01 +0800

    feat: 添加 Vant 库并更新 tab-bar 组件

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="tab-bar">
-    <template v-for="(item, index) in tabbarData">
-      <div class="tab-bar-item" :class="{ active: currentIndex === index }" @click="itemClick(index, item)">
-        <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
-        <img v-else :src="getAssetURL(item.imageActive)" alt="">
-        <span class="text">{{ item.text }}</span>
-      </div>
-    </template>
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabbarData">
+        <van-tabbar-item :to="item.path">
+          <template #default>
+            <span>{{ item.text }}</span>
+          </template>
+          <template #icon>
+            <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
+            <img v-else :src="getAssetURL(item.imageActive) " alt="">
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
   </div>
 </template>
 
@@ -14,47 +20,24 @@
 import tabbarData from "@/assets/data/tabbar.js"
 import { getAssetURL } from "@/utils/load_assets.js"
 import { ref } from "vue";
-import { useRouter } from "vue-router";
 
 const currentIndex = ref(0)
-const router = useRouter()
-const itemClick = (index, item) => {
-  currentIndex.value = index
-  router.push(item.path)
-}
 
 </script>
 
 <style lang="less" scoped>
 .tab-bar {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 50px;
-  display: flex;
+  // 局部定义一个变量: 只针对.tab-bar子元素才生效
+  // --van-tabbar-item-icon-size: 30px !important;
 
-  border-top: 1px solid #f3f3f3;
+  // 找到类, 对类中的CSS属性重写
+  // :deep(.class)找到子组件的类, 让子组件的类也可以生效
+  :deep(.van-tabbar-item__icon) {
+    font-size: 50px;
+  }
 
-  .tab-bar-item {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    &.active {
-      color: var(--primary-color);
-    }
-
-    img {
-      width: 36px;
-    }
-
-    .text {
-      font-size: 12px;
-      margin-top: 2px;
-    }
+  img {
+    height: 26px;
   }
 }
 </style>
